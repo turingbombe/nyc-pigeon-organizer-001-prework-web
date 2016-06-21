6e5aeeb8d255222343cfe212145b63dce5a98dfd
Charlie Cook <turingbombe@gmail.com>
2016-06-21 12:31:21 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  final = {}
+
+  data.each do |k,v|
+    v.each do |k2, v2|
+        v2.each do |x|
+            if final.has_key?(x)
+                if final[x].has_key?(k)
+                    final[x][k] << k2.to_s
+                else
+                    final[x][k] = [k2.to_s]
+                end                
+            else
+                final[x] = {k => [k2.to_s]}
+            end
+        end 
+    end
+  end
+  final
 end
